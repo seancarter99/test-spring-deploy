3f6c23c62ad7252126f676f8d641180a6c91b192
seancarter99 <scarter@skillstorm.com>
2023-05-12 11:54:20 -0400

    Adding project init

---
+++ src/main/java/com/skillstorm/demo/SpringTestDeployApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestDeployApplication.class, args);
+	}
+
+}
